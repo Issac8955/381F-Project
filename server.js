b9d2be55c67e85d7525d7b7cb70ff1b705442ca7
comm899 <comm899jp@gmail.com>
2023-11-09 17:03:41 +0800

    Login function

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const app = express();
+const bodyParser = require('body-parser')
 
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded());
 
 
+//login function
 app.get('/',(req,res) => {
 	res.render('login');
 })
 
+app.post('/login',function(req,res){
+	var username = req.body.username;
+	var password = req.body.password;
+	//Change this to mongodb data
+	if(username = '123'&& password == '123'){
+		res.send('Check login')
+	}
+	else{
+		res.render('Fail to login')
+	}
+})
 
-
-
+//Create the server with port 8099
 app.listen(8099);
